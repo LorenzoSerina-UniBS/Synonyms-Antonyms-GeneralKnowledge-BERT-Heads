52b5949bc100778dc18bec6bdea0f71940c50897
Lorenzo Serina <114875680+LorenzoSerina-UniBS@users.noreply.github.com>
2023-05-29 13:48:51 +0200

    Update loading.py

--- code/loading.py
+++ code/loading.py
@@ -5,7 +5,6 @@ import json
 import pandas as pd
 
 
-#Tieni
 def load_model(bert_path):
     #config = BertConfig.from_pretrained(bert_path, output_hidden_states=True, output_attentions=True)
     tokenizer = BertTokenizer.from_pretrained(bert_path)
@@ -13,7 +12,6 @@ def load_model(bert_path):
     model = BertModel.from_pretrained(bert_path)
     return tokenizer, model
 
-#Tieni
 def comp_matrix(tokenizer, model, sentence):
     max_tokens=model.config.max_position_embeddings
     e = tokenizer.encode(sentence, truncation=True,max_length=max_tokens,add_special_tokens=True)
@@ -25,7 +23,6 @@ def comp_matrix(tokenizer, model, sentence):
     # print(hidden_states[0].shape)
     return attentions, tokens, hidden_states
 
-#Tieni
 def create_environment_and_path(out_dir, name):
     mtx_path = os.path.join(out_dir, name)
     max_path = os.path.join(mtx_path, "max.json")
@@ -34,20 +31,17 @@ def create_environment_and_path(out_dir, name):
         os.mkdir(mtx_path)
     return mtx_path, max_path, sent_path
 
-#Tieni
 def save_in_json(name, path_file):
     out_file = open(path_file, "w", encoding="utf-8")
     json.dump(name, out_file, indent=6, ensure_ascii=False)
     out_file.close()
 
-#Tieni
 def save_sentence(sentence, sent_id, sent_path):
     dic_sent = dict()
     dic_sent['sentence'] = sentence
     dic_sent['sent_id'] = sent_id
     save_in_json(dic_sent, sent_path)
 
-#Tieni
 def save_hidden_state(dir_path, hidden, tokens):
     # dimension (1,n,768)
     for i in range(len(hidden)):
@@ -56,7 +50,6 @@ def save_hidden_state(dir_path, hidden, tokens):
         file_name = dir_path + "/" + "hid_state_layer-" + str(i) + ".csv"
         df.to_csv(file_name, index=False)
 
-#Tieni
 def save_matrix(model,dir_path, tokens, attentions):
     dic_max = dict()
     h=model.config.num_attention_heads
@@ -70,7 +63,6 @@ def save_matrix(model,dir_path, tokens, attentions):
             dic_max[str((i + 1, j + 1))] = df.max().max() + 0
     save_in_json(dic_max, dir_path + "/max.json")
 
-#Tieni
 def load(sentence, sent_id, name, tokenizer, model, out_dir):
     attentions, tokens, hidden = comp_matrix(tokenizer, model, sentence)
     mtx_path, max_path, sent_path = create_environment_and_path(out_dir, name)
@@ -78,25 +70,21 @@ def load(sentence, sent_id, name, tokenizer, model, out_dir):
     save_hidden_state(mtx_path, hidden, tokens)
     save_matrix(model, mtx_path, tokens, attentions)
 
-#Tieni
 def load_from_json(path_file):
     f = open(path_file, 'r')
     data = json.load(f)
     f.close()
     return data
 
-#Tieni
 def get_sentence(out_dir, name):
     dic_sent = load_from_json(os.path.join(os.path.join(out_dir, name), 'sentence.json'))
     return dic_sent['sentence']
 
-#Tieni
 def comp_token(tokenizer, model, sentence):
     max_tokens=model.config.max_position_embeddings
     e = tokenizer.encode(sentence,truncation=True,max_length=max_tokens, add_special_tokens=True)
     return tokenizer.convert_ids_to_tokens(e)
 
-#Tieni
 def get_bert_tokens(mtx_dir, tokenizer, model, sentence):
     bert_path = os.path.join(mtx_dir, "bert_tokens.json")
 
@@ -108,7 +96,6 @@ def get_bert_tokens(mtx_dir, tokenizer, model, sentence):
 
     return bert_tokens
 
-#Tieni
 def load_matrix(out_dir, id_sent, layer, head):
     if head is None:
         file_path = out_dir + "/" + id_sent + "/" + "layer-" + layer + ".csv"
